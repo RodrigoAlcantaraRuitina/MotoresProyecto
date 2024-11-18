aa22ecbbdd3368f024a37863d2792179b4bc18ae
RodrigoAlcantaraRuitiña <9206395@alumnos.ufv.es>
2024-11-18 18:12:31 +0100

    ahora si funciona, intentar arreglar muros

--- Assets/scripts/Movimiento.cs
+++ Assets/scripts/Movimiento.cs
@@ -10,6 +10,9 @@ public class Movimiento : MonoBehaviour
     private float jumpingPower = 16f;
     private bool isFacingRight = true;
 
+    private bool isWallSliding;
+    private float wallSlidingSpeed = 2f;
+
     private bool canDash = true;
     private bool isDashing;
     private float dashingPower = 24f;
@@ -19,7 +22,8 @@ public class Movimiento : MonoBehaviour
     [SerializeField] private Rigidbody2D rb;
     [SerializeField] private Transform groundCheck;
     [SerializeField] private LayerMask groundLayer;
-   
+    [SerializeField] private Transform wallCheck;
+    [SerializeField] private LayerMask wallLayer;
 
     void Update()
     {
@@ -44,6 +48,8 @@ public class Movimiento : MonoBehaviour
               StartCoroutine(Dash());
         }
 
+        WallSlide();
+
         Flip();
     }
 
@@ -62,27 +68,51 @@ public class Movimiento : MonoBehaviour
         return Physics2D.OverlapCircle(groundCheck.position, 0.2f, groundLayer);
     }
 
+    private bool IsWalled() 
+    {
+        return Physics2D.OverlapCircle(wallCheck.position, 0.2f, wallLayer);
+    }
+    private void WallSlide()
+    {
+        if (IsWalled() && !isGrounded() && horizontal != 0f)
+        {
+            isWallSliding = true;
+            rb.linearVelocity = new Vector2(rb.linearVelocity.x, Mathf.Clamp(rb.linearVelocity.y, -wallSlidingSpeed, float.MaxValue));
+        }
+        else
+        {
+            isWallSliding = false;
+        }
+
+    }
     private void Flip()
     {
-        if (isFacingRight && horizontal < 0f || !isFacingRight && horizontal > 1f)
+        if (isFacingRight && horizontal < 0f || !isFacingRight && horizontal > 0f)
         {
             isFacingRight = !isFacingRight;
             Vector3 localScale = transform.localScale;
-            localScale.x *= -1f;
+            localScale.x = isFacingRight ? Mathf.Abs(localScale.x) : -Mathf.Abs(localScale.x);
             transform.localScale = localScale;
         }
     }
 
     private IEnumerator Dash()
     {
+        Debug.Log($"Dashing! FacingRight: {isFacingRight}, Power: {dashingPower}");
+
         canDash = false;
         isDashing = true;
+
         float originalGravity = rb.gravityScale;
         rb.gravityScale = 0f;
-        rb.linearVelocity = new Vector2(transform.localScale.x * dashingPower, 0f);
+
+        rb.linearVelocity = new Vector2((isFacingRight ? 1 : -1) * dashingPower, 0f);
+
         yield return new WaitForSeconds(dashingTime);
+
         rb.gravityScale = originalGravity;
         isDashing = false;
+
         yield return new WaitForSeconds(dashingCooldown);
         canDash = true;
     }
