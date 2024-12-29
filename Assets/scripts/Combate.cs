3158886615a46be8a1d849e550b11e2c2fb81a27
RodrigoAlcantaraRuitina <159939891+RodrigoAlcantaraRuitina@users.noreply.github.com>
2024-12-29 14:47:31 +0100

    Golpe(funciona)

--- Assets/scripts/Combate.cs
+++ Assets/scripts/Combate.cs
@@ -8,23 +8,30 @@ public class Combate : MonoBehaviour
     [SerializeField] private float radioGolpe;
     [SerializeField] private float dañoGolpe;
 
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0)) 
+        {
+            Golpe();
+        }
+    }
     private void Golpe() 
     {
         Collider2D[] objetos = Physics2D.OverlapCircleAll(controladorGolpe.position, radioGolpe);
         
         foreach (Collider2D colisionador in objetos) 
         {
-            //if (colisionador.compareTag("Enemigo")) 
-            //{
-            //colisionador.transform.GetComponent<Enemigo>().TomarDaño(dañoGolpe);
-            //}
+            if (colisionador.CompareTag("Enemigo")) 
+            {
+            colisionador.transform.GetComponent<Enemigo>().TomarDaño(dañoGolpe);
+            }
         }
     
     }
 
-    //private void OnDrawGizmos() 
-    //{
-      //  Gizmos.Color = Color.Red;
-        //Gizmos.DrawWireSphere(controladorGolpe.position, radioGolpe);
-    //}
+    private void OnDrawGizmos() 
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(controladorGolpe.position, radioGolpe);
+    }
 }
