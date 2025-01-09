1686f93005e12aadd85305d0bc84146a157964e1
RodrigoAlcantaraRuitina <159939891+RodrigoAlcantaraRuitina@users.noreply.github.com>
2025-01-09 02:43:35 +0100

    FINAL

--- Assets/scripts/final.cs
+++ Assets/scripts/final.cs
@@ -1,16 +1,16 @@
 using UnityEngine;
-
+using UnityEngine.SceneManagement; // Necesario para cambiar de escena
 public class final : MonoBehaviour
 {
-    // Método llamado al detectar una colisión
-    private void OnCollisionEnter(Collision collision)
+    // Método llamado al detectar una colisión 2D
+    private void OnCollisionEnter2D(Collision2D collision)
     {
         // Verificar si el objeto que colisiona tiene el tag "Player"
         if (collision.collider.CompareTag("Player"))
         {
-            // Cerrar la aplicación
-            Debug.Log("Jugador detectado. Cerrando la aplicación...");
-            Application.Quit();
+            // Cargar la escena "menu"
+            Debug.Log("Jugador detectado. Cargando la escena 'menu'...");
+            SceneManager.LoadScene("menu");
         }
     }
 }
